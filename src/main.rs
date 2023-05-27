ad43927b4094842ed42756b013c09bd454c67ccd
Zeyphros <robin@decker.cx>
2023-05-27 11:35:08 +0200

    linearly interpolate speed and power between track records

--- src/main.rs
+++ src/main.rs
@@ -1,136 +1,171 @@
-//! This example prints information on every video and audio frames
-//! contained in a matroska file.
+mod parser;
 
-use std::collections::HashMap;
-use std::fs::File;
-use std::path::PathBuf;
+use std::ptr::{null, null_mut};
 
-use clap::Parser;
+use chrono::{offset, DateTime, Duration, Local, NaiveDate, NaiveDateTime, TimeZone, Utc};
+use std::env;
+use std::fmt::Error;
 
-use av_codec::common::CodecList;
-use av_codec::decoder::Codecs as DecCodecs;
-use av_codec::decoder::Context as DecContext;
-use av_data::frame::{ArcFrame, MediaKind};
-use av_format::buffer::{AccReader, Buffered};
-use av_format::demuxer::{Context, Demuxer, Event};
+use opencv::core::{Point, Scalar, Size, Vector, BORDER_DEFAULT};
+use opencv::gapi::GMat;
+use opencv::imgproc::put_text;
+use opencv::imgproc::INTER_LINEAR;
+use opencv::prelude::*;
+use opencv::videoio::{VideoCapture, VideoCaptureProperties, VideoWriter, CAP_ANY, CAP_FFMPEG};
+use opencv::{gapi, highgui, Result};
 
-use matroska::demuxer::MkvDemuxer;
+fn draw_text(img: &mut Mat, text: &str, line: i32, frame_height: i32) -> Result<()> {
+    let font = 1;
+    let scale = 5.0;
+    let color = Scalar::new(255., 255., 255., 0.);
+    let thickness = 3;
+    let line_type = 8;
+    let bottom_left_origin = false;
 
-use av_vorbis::decoder::VORBIS_DESCR;
-use libopus::decoder::OPUS_DESCR;
-use libvpx::decoder::VP9_DESCR;
+    put_text(
+        img,
+        text,
+        Point::new(10, frame_height - (80 * line)),
+        font,
+        scale,
+        color,
+        thickness,
+        line_type,
+        bottom_left_origin,
+    )?;
 
-// This function decodes a single frame using the most appropriate decoder
-fn decode_single_frame<D: Demuxer, R: Buffered>(
-    demuxer: &mut Context<D, R>,
-    decoders: &mut HashMap<isize, DecContext>,
-) -> Result<Option<ArcFrame>, String> {
-    // The demuxer reads which event has occurred
-    match demuxer.read_event() {
-        // If a new packet has been found, decode it
-        Ok(event) => match event {
-            Event::NewPacket(pkt) => {
-                // Choose the right decoder for the packet
-                if let Some(decoder) = decoders.get_mut(&pkt.stream_index) {
-                    decoder.send_packet(&pkt).unwrap();
-                    Ok(decoder.receive_frame().ok())
-                } else {
-                    // If a packet cannot be decoded, it will be skipped
-                    println!("Skipping packet at index {}", pkt.stream_index);
-                    Ok(None)
-                }
-            }
-            // When the EOF is reached, the decoding process is stopped
-            Event::Eof => {
-                println!("EOF reached.");
-                Err("EOF reached".to_owned())
-            }
-            _ => {
-                // If an unsupported event occurs,
-                // the decoding process is stopped
-                println!("Unsupported event {:?}", event);
-                Err("Unsupported event".to_owned())
-            }
-        },
-        Err(err) => {
-            // If there are no more events, the decoding process is stopped
-            println!("No more events {:?}", err);
-            Err("No more events".to_owned())
-        }
-    }
+    Ok(())
 }
 
-#[derive(Parser, Debug)]
-#[clap(
-    name = "frames-info",
-    version,
-    author,
-    about = "Gets information on audio and video frames"
-)]
-struct Opts {
-    /// Sets the matroska file to analyze
-    #[clap(long, short, value_parser)]
-    input: PathBuf,
-}
+pub fn main() -> Result<(), opencv::Error> {
+    let linearization = parser::main().expect("Failed to read");
 
-fn main() {
-    let opts = Opts::parse();
+    let mut tracking_index = 0i64;
 
-    // Open the matroska file
-    let reader = File::open(opts.input).unwrap();
+    let video_file_path = "/run/media/robin/disk/DCIM/100_CF12/CYQ_0368.MP4";
 
-    // Create a buffer of size 4096MB to contain matroska data
-    let ar = AccReader::with_capacity(4 * 1024, reader);
+    let mut cap = VideoCapture::from_file(video_file_path, CAP_FFMPEG)?;
+    let video_ffprobe = match ffprobe::ffprobe(video_file_path) {
+        Ok(v) => dbg!(v),
+        Err(e) => panic!("ffprobe error: {}", e),
+    };
 
-    // Set the type of demuxer, in this case, a matroska demuxer
-    let mut demuxer = Context::new(MkvDemuxer::new(), ar);
+    let creation_time_string = match video_ffprobe.streams[0].tags.as_ref() {
+        Some(tags) => tags.creation_time.as_ref().unwrap(),
+        None => panic!("No creation time"),
+    };
 
-    // Read matroska headers
-    demuxer
-        .read_headers()
-        .expect("Cannot parse the format headers");
+    let creation_time: NaiveDateTime =
+        NaiveDateTime::parse_from_str(creation_time_string, "%Y-%m-%dT%H:%M:%S%.fZ").unwrap();
 
-    // Set the list of supported decoders
-    let decoders = DecCodecs::from_list(&[VP9_DESCR, OPUS_DESCR, VORBIS_DESCR]);
+    println!("creation_time: {}", creation_time);
 
-    // Save the context of each decoder (its configuration)
-    let mut decs: HashMap<isize, DecContext> = HashMap::with_capacity(2);
+    let mut current_track_record = &linearization[tracking_index as usize];
 
-    // Iterate over the streams contained in a matroska file
-    for stream in &demuxer.info.streams {
-        // Get the id of the codec associated to the stream
-        if let Some(ref codec_id) = stream.params.codec_id {
-            // Get the right decoder from the decoder list using the id of a codec
-            if let Some(mut ctx) = DecContext::by_name(&decoders, codec_id) {
-                // Add stream extradata to the decoder
-                if let Some(ref extradata) = stream.params.extradata {
-                    ctx.set_extradata(extradata);
-                }
-                // Configure a decoder
-                ctx.configure().expect("Codec configure failed");
-                // Save the context of a decoder
-                decs.insert(stream.index as isize, ctx);
-            }
-        }
+    println!("First track time: {:?}", current_track_record.timestamp);
+
+    let mut next_track_record = &linearization[(tracking_index + 1) as usize];
+
+    while creation_time > current_track_record.timestamp
+        && tracking_index < linearization.len() as i64 - 1
+    {
+        current_track_record = &linearization[tracking_index as usize];
+        next_track_record = &linearization[(tracking_index + 1) as usize];
+
+        tracking_index += 1;
     }
 
-    let mut frame_idx = 0;
-    // Iterate over the decoded frames
-    while let Ok(data) = decode_single_frame(&mut demuxer, &mut decs) {
-        // Check if the decoding process of a frame went well
-        if let Some(frame) = data {
-            // Print the kind of a decoded frame
-            match frame.kind {
-                MediaKind::Video(_) => {
-                    println!("Frame {} is a video frame\n", frame_idx);
-                }
-                MediaKind::Audio(_) => {
-                    println!("Frame {} is an audio frame\n", frame_idx);
-                }
-            }
-            // Print the content of a frame
-            println!("Decoded {:?}\n\n", frame);
-            frame_idx += 1;
-        }
+    print!("Last track time: {:?}", current_track_record.timestamp);
+
+    if tracking_index == linearization.len() as i64 - 1 {
+        panic!("No track record found");
     }
+
+    let fps = cap.get(5).unwrap();
+    let frame_height = cap.get(4).unwrap() as i32;
+    let frame_width = cap.get(3).unwrap() as i32;
+
+    let mut video_writer = VideoWriter::new(
+        "/home/robin/test.mkv",
+        VideoWriter::fourcc('h', '2', '6', '4').unwrap().into(),
+        fps,
+        Size::new(frame_width, frame_height),
+        true,
+    )?;
+
+    let mut frame_num = 0;
+    let mut last_track_time = 0f64;
+
+    let frame_count = cap.get(7).unwrap() as i32;
+
+    loop {
+        frame_num += 1;
+
+        let time_offset = frame_num as f64 / fps;
+
+        if creation_time + Duration::seconds(time_offset as i64) >= next_track_record.timestamp {
+            tracking_index += 1;
+
+            current_track_record = &linearization[tracking_index as usize];
+            next_track_record = &linearization[(tracking_index + 1) as usize];
+
+            last_track_time = time_offset;
+        }
+
+        //println!("Frame: {} time: {}", frame_num, time_offset);
+
+        let mut input_frame = Mat::default();
+        let has_next = cap.read(&mut input_frame)?;
+
+        if !has_next {
+            break;
+        }
+
+        let time_delta_seconds = (next_track_record.timestamp - current_track_record.timestamp)
+            .num_microseconds()
+            .unwrap() as f64
+            * 0.000001;
+
+        let current_power = current_track_record.power.unwrap_or(0) as f64;
+        let next_power = next_track_record.power.unwrap_or(0) as f64;
+        let power_delta = next_power - current_power;
+
+        // Scale linearly between current and next power
+        let power =
+            current_power + power_delta * ((time_offset - last_track_time) / time_delta_seconds);
+
+        let current_speed = current_track_record.enhanced_speed * 3.6;
+        let next_speed = next_track_record.enhanced_speed * 3.6;
+        let speed_delta = next_speed - current_speed;
+
+        let speed = current_speed + speed_delta * ((time_offset - last_track_time) / time_delta_seconds);
+
+        let heart_rate = current_track_record.hr.unwrap_or(0);
+
+        draw_text(
+            &mut input_frame,
+            format!("Power: {:.2} watts", power).as_str(),
+            1,
+            frame_height,
+        )?;
+
+        draw_text(
+            &mut input_frame,
+            format!("Heart Rate: {} bpm", heart_rate).as_str(),
+            2,
+            frame_height,
+        )?;
+
+        draw_text(
+            &mut input_frame,
+            format!("Speed: {:.2} km/h", speed).as_str(),
+            3,
+            frame_height,
+        )?;
+        video_writer.write(&input_frame)?;
+    }
+
+    video_writer.release()?;
+
+    Ok(())
 }
